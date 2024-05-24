1443b644c1bbc6524de915bc8dba2b5d48eac8d2
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:34:37 -0400

    style: fix some stylua warnings

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,10 +1,12 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
+-- bootstrap lazy.nvim
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  -- bootstrap lazy.nvim
   -- stylua: ignore
   vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
 end
+
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 local local_plugins_exist, _ = pcall(require, "local.plugins")
@@ -14,8 +16,10 @@ require("lazy").setup({
         {
             "LazyVim/LazyVim",
             import = "lazyvim.plugins",
-            -- I'd rather add them incrementally.
-            opts = { defaults = { keymaps = false } },
+            opts = {
+                -- I'd rather add them incrementally.
+                defaults = { keymaps = false },
+            },
         },
         { import = "plugins" },
         local_plugins_exist and { import = "local.plugins" } or nil,
