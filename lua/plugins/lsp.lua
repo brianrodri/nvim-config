8068dc99ef4bf0ef5be2213cb3205169c16c9dc2
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:34:42 -0400

    feat: remove organize imports shortcut from lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,27 +1,5 @@
 local local_jdtls_exists, _ = pcall(require, "local.jdtls")
 
 return {
-    {
-        "neovim/nvim-lspconfig",
-        opts = function(_, opts)
-            vim.tbl_deep_extend("force", opts.servers, {
-                ["tsserver.keys"] = {
-                    {
-                        "<leader><C-y>",
-                        function()
-                            vim.lsp.buf.code_action({
-                                apply = true,
-                                context = {
-                                    only = { "source.organizeImports.ts" },
-                                    diagnostics = {},
-                                },
-                            })
-                        end,
-                        desc = "Organize Imports",
-                    },
-                },
-            })
-        end,
-    },
     local_jdtls_exists and { import = "local.jdtls" } or nil,
 }
