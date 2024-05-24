4b85c7523d6623e5ac87fa8ca5aab36c16f2fdb6
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:36:04 -0400

    style: always apply autoformatting

--- lua/config/autocmds.lua
+++
@@ -1,5 +0,0 @@
--- Autoformat files that bother me
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = { "lua", "html", "xml", "css", "json", "txt" },
-    callback = function() vim.b.autoformat = true end,
-})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.opt.cursorline = true
 vim.opt.number = true
 vim.opt.relativenumber = false
-vim.g.autoformat = false
+vim.g.autoformat = true
 vim.wo.wrap = false
 vim.opt.mouse = "a"
 vim.opt.signcolumn = "yes"
