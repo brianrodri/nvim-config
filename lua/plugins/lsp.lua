918e185649f04c94bcce7e0daa82d463c6b3f79e
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:34:44 -0400

    lsp(bashls): run bashls on zsh files

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,5 +1,17 @@
 local local_jdtls_exists, _ = pcall(require, "local.jdtls")
 
 return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            -- Useful for debugging formatter issues
+            format_notify = true,
+            servers = {
+                bashls = {
+                    filetypes = { "zsh" },
+                },
+            },
+        },
+    },
     local_jdtls_exists and { import = "local.jdtls" } or nil,
 }
