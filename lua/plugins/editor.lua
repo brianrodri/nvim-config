3a7b4d9c798878b38cae4a2432c406acbe6e3ad9
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:34:43 -0400

    chore(editor): remove useless dependency

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -28,7 +28,6 @@ return {
 
     {
         "hrsh7th/nvim-cmp",
-        dependencies = { "hrsh7th/cmp-emoji" },
         ---@param opts cmp.ConfigSchema
         opts = function(_, opts)
             local luasnip = require("luasnip")
