476771e1856fbb547d8c9157bd41b435ac13f1e5
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:34:42 -0400

    feat: support neotest-java

--- lua/plugins/testing.lua
+++ lua/plugins/testing.lua
@@ -1,17 +1,15 @@
 return {
     {
-        "marilari88/neotest-vitest",
+        "nvim-neotest/neotest",
+        opts = function(_, opts)
+            vim.list_extend(opts.adapters, { "neotest-vitest", "neotest-java" })
+        end,
         dependencies = {
-            {
-                "nvim-neotest/neotest",
-                opts = function(_, opts)
-                    vim.tbl_deep_extend("force", opts.status, { signs = false, virtual_text = false })
-                    vim.list_extend(opts.adapters, { "neotest-vitest" })
-                end,
-            },
+            "rcasia/neotest-java",
+            "marilari88/neotest-vitest",
             "nvim-tree/nvim-web-devicons",
             "nvim-treesitter/nvim-treesitter",
             "nvim-lua/plenary.nvim",
-        },
+        }
     },
 }
