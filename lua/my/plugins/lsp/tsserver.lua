1f60941ddb139c972be6231716c974a5a5317379
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:33:32 -0400

    Setup tsserver, pyright, and json

---
+++ lua/my/plugins/lsp/tsserver.lua
@@ -0,0 +1,34 @@
+return {
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.test.core" },
+    {
+        "jose-elias-alvarez/typescript.nvim",
+        init = function()
+            require("lazyvim.util").on_attach(function(_, bufnr)
+                -- stylua: ignore
+                vim.keymap.set("n", "<leader><C-Y>", "TypescriptOrganizeImports", { buffer = bufnr, desc = "Organize Imports" })
+                vim.keymap.set("n", "<leader><C-R>", "TypescriptRenameFile", { buffer = bufnr, desc = "Rename Module" })
+            end)
+        end,
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            opts = function(_, opts)
+                vim.tbl_deep_extend("force", opts.servers, { tsserver = {} })
+                vim.tbl_deep_extend("force", opts.setup, {
+                    tsserver = function(_, setup_opts)
+                        require("typescript").setup({ server = setup_opts })
+                        return true
+                    end,
+                })
+            end,
+        },
+    },
+    {
+        "nvim-neotest/neotest",
+        dependencies = { "nvim-neotest/neotest-jest", "nvim-tree/nvim-web-devicons" },
+        opts = function(_, opts)
+            vim.list_extend(opts.adapters, { "neotest-jest" })
+        end,
+    },
+}
