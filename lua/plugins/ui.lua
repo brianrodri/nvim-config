b9a329d0c8bbdcb62163833b2fe9a342345c157f
Brian Rodriguez <brian@brianrodri.com>
2024-05-24 15:34:22 -0400

    feat(colorscheme): opinionated gruvbox-material settings.

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -7,6 +7,14 @@ return {
         config = function()
             vim.o.termguicolors = true
             vim.g.gruvbox_material_background = "hard"
+            vim.g.gruvbox_material_diagnostic_line_highlight = 1
+            vim.g.gruvbox_material_diagnostic_text_highlight = 1
+            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+            vim.g.gruvbox_material_enable_bold = 1
+            vim.g.gruvbox_material_enable_italic = 1
+            vim.g.gruvbox_material_foreground = "original"
+            vim.g.gruvbox_material_statusline_style = "original"
+            vim.g.gruvbox_material_ui_contrast = "high"
         end,
     },
     { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox-material" } },
