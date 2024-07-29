7a55969a56e6552ec3389ac1afd3bf740c0ab6be
Brian Rodriguez <brian@brianrodri.com>
2024-07-29 10:09:42 -0400

    refactor: update which-key mappings to the v3 schema

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -3,51 +3,41 @@ local lazyvim_util = require("lazyvim.util")
 local which_key = require("which-key")
 
 local which_key_mappings = {
-    ZA = { ":qa!<CR>", "Force quit" },
-
-    ["[o"] = { name = "+Enable options" },
-    ["[oh"] = { function() vim.o.hlsearch = true end, "Enable highlight search" },
-    ["[of"] = { function() vim.b.autoformat = true end, "Enable auto-format" },
-    ["[ot"] = { function() vim.lsp.inlay_hint.enable(true) end, "Enable inlay hints" },
-
-    ["]o"] = { name = "+Disable options" },
-    ["]oh"] = { function() vim.o.hlsearch = false end, "Disable highlight search" },
-    ["]of"] = { function() vim.b.autoformat = false end, "Disable auto-format" },
-    ["]ot"] = { function() vim.lsp.inlay_hint.enable(false) end, "Disable inlay hints" },
-
-    ["<leader>"] = {
-        e = { ":e! %<CR>", "Force re-open" },
-        w = { ":w!<CR>", "Force write" },
-        X = { ":bdelete!<CR>", "Force close" },
-
-        f = { name = "+find" },
-        fc = { lazyvim_util.pick.config_files(), "Config" },
-        fd = { lazyvim_util.pick("git_status"), "Diffs" },
-        ff = { lazyvim_util.pick("files"), "Files" },
-        fF = { lazyvim_util.pick("find_files"), "All Files" },
-        fr = { lazyvim_util.pick("oldfiles"), "Recent files" },
-        ft = { ":Neotree toggle<CR>", "Neotree" },
-        fg = { ":Neotree git_status toggle<CR>", "Neotree" },
-        fb = { ":Neotree buffers toggle<CR>", "Neotree" },
-        ["f/"] = { lazyvim_util.pick("live_grep"), "Grep" },
-        ["f:"] = { lazyvim_util.pick("command_history"), "Command History" },
-
-        gg = { function() lazy_util.float_term("lazygit") end, "lazygit" },
-
-        l = { name = "+lazy" },
-        ll = { ":Lazy<CR>", "LazyVim" },
-        le = { ":LazyExtras<CR>", "LazyExtras" },
-
-        t = { name = "+test" },
-        ["tt"] = { function() require("neotest").run.run() end, "Run Nearest" },
-        ["tT"] = { function() require("neotest").run.run(vim.fn.expand("%")) end, "Run File" },
-        ["tg"] = { function() require("neotest").run.run(vim.uv.cwd()) end, "Run All Test Files" },
-
-        ["<C-h>"] = { ":topleft vsplit<CR>", "Split left" },
-        ["<C-j>"] = { ":belowright split<CR>", "Split down" },
-        ["<C-k>"] = { ":topleft split<CR>", "Split up" },
-        ["<C-l>"] = { ":belowright vsplit<CR>", "Split right" },
-    },
+    { "<leader><C-h>", ":topleft vsplit<CR>", desc = "Split left" },
+    { "<leader><C-j>", ":belowright split<CR>", desc = "Split down" },
+    { "<leader><C-k>", ":topleft split<CR>", desc = "Split up" },
+    { "<leader><C-l>", ":belowright vsplit<CR>", desc = "Split right" },
+    { "<leader>X", ":bdelete!<CR>", desc = "Force close" },
+    { "<leader>e", ":e! %<CR>", desc = "Force re-open" },
+    { "<leader>f", group = "find" },
+    { "<leader>f/", lazyvim_util.pick("live_grep"), desc = "Grep" },
+    { "<leader>f:", lazyvim_util.pick("command_history"), desc = "Command History" },
+    { "<leader>fF", lazyvim_util.pick("find_files"), desc = "All Files" },
+    { "<leader>fb", ":Neotree buffers toggle<CR>", desc = "Neotree" },
+    { "<leader>fc", lazyvim_util.pick.config_files(), desc = "Config" },
+    { "<leader>fd", lazyvim_util.pick("git_status"), desc = "Diffs" },
+    { "<leader>ff", lazyvim_util.pick("files"), desc = "Files" },
+    { "<leader>fg", ":Neotree git_status toggle<CR>", desc = "Neotree" },
+    { "<leader>fr", lazyvim_util.pick("oldfiles"), desc = "Recent files" },
+    { "<leader>ft", ":Neotree toggle<CR>", desc = "Neotree" },
+    { "<leader>gg", function() lazy_util.float_term("lazygit") end, desc = "lazygit" },
+    { "<leader>l", group = "lazy" },
+    { "<leader>le", ":LazyExtras<CR>", desc = "LazyExtras" },
+    { "<leader>ll", ":Lazy<CR>", desc = "LazyVim" },
+    { "<leader>t", group = "test" },
+    { "<leader>tT", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
+    { "<leader>tg", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
+    { "<leader>tt", function() require("neotest").run.run() end, desc = "Run Nearest" },
+    { "<leader>w", ":w!<CR>", desc = "Force write" },
+    { "ZA", ":qa!<CR>", desc = "Force quit" },
+    { "[o", group = "enable" },
+    { "[of", function() vim.b.autoformat = true end, desc = "auto-format" },
+    { "[oh", function() vim.o.hlsearch = true end, desc = "highlight search" },
+    { "[ot", function() vim.lsp.inlay_hint.enable(true) end, desc = "inlay hints" },
+    { "]o", group = "disable" },
+    { "]of", function() vim.b.autoformat = false end, desc = "auto-format" },
+    { "]oh", function() vim.o.hlsearch = false end, desc = "highlight search" },
+    { "]ot", function() vim.lsp.inlay_hint.enable(false) end, desc = "inlay hints" },
 }
 
 return {
@@ -56,7 +46,7 @@ return {
         event = "VeryLazy",
         config = function()
             which_key.setup({ plugins = { spelling = true } })
-            which_key.register(which_key_mappings)
+            which_key.add(which_key_mappings)
         end,
     },
 }
