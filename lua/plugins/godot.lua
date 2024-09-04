371adcf5cfbaf9cce1539e33c20a36728c4c0eff
Brian Rodriguez <brian@brianrodri.com>
2024-09-04 17:15:24 -0400

    feat(godot): add lsp/dap configs for godot files

---
+++ lua/plugins/godot.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                gdscript = {
+                    filetypes = { "gd", "gdscript", "gdscript3" },
+                },
+            },
+        },
+    },
+    {
+        "mfussenegger/nvim-dap",
+        opts = function()
+            local dap = require("dap")
+            if not dap.adapters["godot"] then
+                dap.adapters["godot"] = {
+                    type = "server",
+                    host = "127.0.0.1",
+                    port = 6006,
+                }
+                dap.configurations.gdscript = {
+                    {
+                        type = "godot",
+                        request = "launch",
+                        name = "Launch scene",
+                        project = "${workspaceFolder}",
+                        launch_scene = true,
+                    },
+                }
+            end
+        end,
+    },
+}
