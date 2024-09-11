f5e56ff6b7751463da03b1edb5e5476bf7f8fefe
Brian Rodriguez <brian@brianrodri.com>
2024-09-11 18:52:38 -0400

    chore: disable unused providers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,8 @@ vim.opt.mouse = "a"
 vim.opt.signcolumn = "yes"
 
 vim.g.autoformat = false
+
+-- Disable unused providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
