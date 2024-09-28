3157a192e42a27ac0b0c76985d29ea62d48387d8
Brian Rodriguez <brian@brianrodri.com>
2024-09-28 12:51:08 -0400

    feat(neotest): add vitest and jest runners

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,5 +13,15 @@ return {
             },
         },
     },
+    {
+        "nvim-neotest/neotest",
+        dependencies = { "marilari88/neotest-vitest", "nvim-neotest/neotest-jest" },
+        opts = {
+            adapters = {
+                ["neotest-vitest"] = {},
+                ["neotest-jest"] = {},
+            },
+        },
+    },
     local_jdtls_exists and { import = "local.jdtls" } or nil,
 }
