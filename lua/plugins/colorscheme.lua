b9403a83b45653440396de834f51406beab94834
Brian Rodriguez <brian@brianrodri.com>
2024-11-15 02:07:54 -0500

    fix: everforest colors

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,16 @@
 return {
     {
         "sainnhe/everforest",
-        priority = 1000,
         lazy = false,
+        priority = 1000,
         config = function()
-            vim.o.termguicolors = true
             vim.o.background = "dark"
             vim.g.everforest_background = "hard"
-            vim.g.everforest_ui_contrast = "high"
-            vim.g.everforest_enable_italic = 1
             vim.g.everforest_better_performance = 1
+            vim.g.everforest_enable_italic = 1
+            vim.g.everforest_float_style = "dim"
+            vim.g.everforest_ui_contrast = "high"
             vim.cmd.colorscheme("everforest")
         end,
     },
-
-    { "nvim-lualine/lualine.nvim", opts = { theme = "everforest" } },
 }
