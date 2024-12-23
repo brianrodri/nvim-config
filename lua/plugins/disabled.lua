03452f22373209f606acd2db9d60a6915c0eb4b1
Brian Rodriguez <brian@brianrodri.com>
2024-12-23 12:56:06 -0500

    feat!: disable mini.pairs, it causes more trouble than its worth to me

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -7,6 +7,9 @@ return {
 
     ---@begin Disabled plugins
 
+    -- It gets in my way more than helping.
+    { "echasnovski/mini.pairs", enabled = false },
+
     -- Gotta think of an intuitive key binding for this, I don't like the default.
     { "folke/flash.nvim", enabled = false },
 
