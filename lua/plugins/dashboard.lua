6ebf942fdf772eb6726b2160be83fc74e3967e3f
Brian Rodriguez <brian@brianrodri.com>
2025-01-07 20:26:14 -0500

    feat: configure dashboard items

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -31,6 +31,20 @@ return {
                         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⢿⣿⣿⣿⣿⣷⣶⣶⣿⠟⠁⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
                         "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠟⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
                     }):join("\n"),
+                    -- stylua: ignore
+                    ---@type snacks.dashboard.Item[]
+                    keys = {
+                        { icon = "󰝒 ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        { icon = "󱋡 ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = "󰱼 ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                        { icon = "󰨭 ", key = "p", desc = "Find Project", action = ":Telescope projects" },
+                        { icon = "󱎸 ", key = "/", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                        { icon = " ", key = "g", desc = "Lazygit", action = ":lua Snacks.lazygit()" },
+                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    },
                 },
             },
         },
